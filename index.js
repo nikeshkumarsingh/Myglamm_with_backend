f45d1eedded8f68a68ea999e2815035b5f0949d9
Nikesh Kumar Singh <97428897+nikeshkumarsingh@users.noreply.github.com>
2022-03-30 19:15:05 +0530

    schema

---
+++ index.js
@@ -0,0 +1,11 @@
+const express=require("express");
+const cors=require("cors");
+const productcontroller=require("./controller/Product.controller");
+const skincarecontroller=require("./controller/skincare.controller")
+
+ const app=express();
+ app.use(cors());
+ app.use(express.json());
+ app.use("/products",productcontroller);
+ app.use("/skincares",skincarecontroller);
+ module.exports=app;
---
+++ models/Product.model.js
@@ -0,0 +1,21 @@
+const mongoose=require("mongoose")
+
+const productSchema =new mongoose.Schema(
+    {
+        Name:{type:String,required:true},
+        Price:{type:Number,required:true},
+        Description:{type:String,required:true},
+        Use:{type:String,required:true},
+        benifit:{type:String,required:true},
+        exproperty:{type:String,required:true},
+        Image1:{type:String,required:true},
+        Image2:{type:String,required:true},
+        Image3:{type:String,required:true},
+        Image4:{type:String,required:true},
+        Image5:{type:String,required:true},
+    },{
+        timestamps:true,
+    }
+)
+
+module.exports=mongoose.model("product",productSchema)
