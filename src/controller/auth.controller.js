37255c7dff9553e8e70360808ace13beeab69acb
nikeshkumarsingh <nikeshmasai1947@gmail.com>
2022-04-02 02:14:03 +0530

    pre final update

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -17,7 +17,7 @@ const register = async (req, res) => {
     user = await User.create(req.body);
 
     const token = genrateToken(user);
-    return res.status(200).redirect("http://127.0.0.1:5501/start_pro/frontend/index.html");
+    return res.status(200).redirect(req.body.link);
   } catch (err) {
     res.status(400).send({ message: err.message });
   }
@@ -37,7 +37,8 @@ const login = async (req, res) => {
     }
 
     const token = genrateToken(user);
-    return res.status(200).redirect("http://127.0.0.1:5501/start_pro/frontend/index.html");
+    console.log(req.body.email)
+    return res.status(200).redirect(req.body.link);
   } catch (err) {
     res.status(400).send({ message: err.message });
   }
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -5,6 +5,7 @@ const userSchema = mongoose.Schema(
   {
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    link:{type:String,required:true},
   },
   {
     timestamps: true,
