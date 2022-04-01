c3d1b7e7b73127cb8abe1aa3d539eec640448dd3
Rahul Baghel <rahulbaghel6211@gmail.com>
2022-04-01 17:03:03 +0530

    pushlipstick

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const express = require("express");
 const cors = require("cors");
 const productcontroller = require("./controller/product.controller");
@@ -16,6 +17,34 @@ app.use("/products", productcontroller);
 app.use("/makeups", makeupcontroller);
 app.use("/lipsticks", lipstickController);
 app.use("/haircare", haircareController);
+=======
+const express=require("express");
+const cors=require("cors");
+const productcontroller=require("./controller/product.controller");
+const skincarecontroller=require("./controller/skincare.controller");
+const makeupcontroller=require("./controller/MAKEUP.controller");
+const lipstickcontroller=require("./controller/lipstick.controllers");
+const usersController = require("./controller/users.controllers");
+const registrationController = require("./controller/registration.controller ");
+const {register,login}=require("./controller/auth.controller")
+ const app=express();
+ app.use(cors());
+ app.use(express.json());
+ app.use("/products",productcontroller);
+ app.use("/skincares",skincarecontroller);
+app.use("/users", usersController); 
+app.post("/register",register)
+app.post("/login",login)
+app.use("/registration", registrationController)
+ app.use("/makeups",makeupcontroller);
+ app.use("/lipstick",lipstickcontroller)
+
+ 
+module.exports=app;
+
+
+
+>>>>>>> Stashed changes
 
 app.use("/skincares", skincarecontroller);
 app.use("/users", usersController);
