e74d2f826abd14ad9e18027fc11bbff215371796
nikeshkumarsingh <nikeshmasai1947@gmail.com>
2022-04-01 12:57:19 +0530

    delete extra

--- src/controller/skincare.controller.js
+++ src/controller/skincare.controller.js
@@ -10,20 +10,7 @@ router.get("",async (req,res)=>{
         return res.status(500).send({message:err.message});
     }
 });
-// router.post("",upload.single("profilePic"),async (req,res)=>{
-//     try{
-//         const user= await User.create({
-//             firstName:req.body.firstName,
-//             lastName:req.body.lastName,
-//             profilepic:req.file.path,
-//         });
-//         return res.status(200).send(user);
-//     }
-//     catch(err){
-//         console.log(err)
-//         return res.status(500).send({message:err.message})
-//     }
-// })
+
 router.post("",async(req,res)=>{
    try{
        
