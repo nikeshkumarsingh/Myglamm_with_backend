1153e22326861f3dcde366bb3cb7058bcfc781b4
Vaibhav Girish Ginnalwar <97450767+Vaibhav0702@users.noreply.github.com>
2022-04-01 16:22:33 +0530

    Updated

---
+++ src/controller/lipstick.controller.js
@@ -0,0 +1,26 @@
+const  express=require("express");
+const lipstick=require("../models/lipsticks.model");
+
+const router=express.Router();
+router.get("",async (req,res)=>{
+    try{
+        const lipstick=await lipstick.find().lean().exec();
+        return res.status(200).send(lipstick)
+    }catch(err){
+        return res.status(500).send({message:err.message});
+    }
+});
+
+router.post("",async(req,res)=>{
+   try{
+       
+       const lipstick =await lipstick.create(req.body);
+       return res.status(200).send(lipstick)
+   } 
+   catch(err){
+    return res.status(500).send({message:err.message})
+   }
+});
+
+
+module.exports=router;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const cors=require("cors");
 const productcontroller=require("./controller/product.controller");
 const skincarecontroller=require("./controller/skincare.controller");
 const makeupcontroller=require("./controller/MAKEUP.controller");
+const lipstickController = require("./controller/lipstick.controller");
+const haircareController = require("./controller/haircare.Controller");
+
 
 const usersController = require("./controller/users.controllers");
 const registrationController = require("./controller/registration.controller ");
@@ -11,12 +14,17 @@ const {register,login}=require("./controller/auth.controller")
  app.use(cors());
  app.use(express.json());
  app.use("/products",productcontroller);
+ app.use("/makeups",makeupcontroller);
+ app.use("/lipsticks",lipstickController);
+ app.use("/haircare",haircareController);
+
+
  app.use("/skincares",skincarecontroller);
 app.use("/users", usersController); 
 app.post("/register",register)
 app.post("/login",login)
 app.use("/registration", registrationController)
- app.use("/makeups",makeupcontroller);
+app.use("/makeups",makeupcontroller);
 
 module.exports=app;
 
