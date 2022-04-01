dba92b38f5ed0f6af6573e4245dee9f74fbdbd56
Vaibhav Girish Ginnalwar <97450767+Vaibhav0702@users.noreply.github.com>
2022-04-01 17:55:19 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const productcontroller = require("./controller/product.controller");
 const skincarecontroller = require("./controller/skincare.controller");
 const makeupcontroller = require("./controller/MAKEUP.controller");
 const haircareController = require("./controller/haircare.Controller");
+const lipstickController = require("./controller/lipstick.controller");
+
 const usersController = require("./controller/users.controllers");
 const registrationController = require("./controller/registration.controller ");
 const { register, login } = require("./controller/auth.controller");
@@ -15,6 +17,9 @@ app.use(express.json());
 app.use("/products", productcontroller);
 app.use("/makeups", makeupcontroller);
 app.use("/haircare", haircareController);
+app.use("/lipsticks", lipstickController);
+
+
 app.use("/skincares", skincarecontroller);
 app.use("/users", usersController);
 app.post("/register", register);
